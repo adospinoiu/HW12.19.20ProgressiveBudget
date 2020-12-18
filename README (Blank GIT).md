01e6aab612442d12c2810cb5bff17cffe617c7f7
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-12-18 06:37:26 -0700

    Created webpack.config.js file and configured package.json file

--- README (Blank GIT).md
+++ README (Blank GIT).md
@@ -1,1 +1,15 @@
 # HW12.19.20ProgressiveBudget
+
+#1 Installed with npm install 'if-env'
+
+#2 Installed with npm install 'concurrently'
+
+#3 Installed with npm install 'webpack webpack-cli -D'
+
+#4 Created file 'webpack.config.js'
+- In webpack.config.js file added
+    - entry, output, mode, module sections
+    - plugins
+
+#5 Added "prestart": "npm run webpack" in package.json
+
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+    entry: {
+        app: './public/index.js'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new WebpackPwaManifest({
+            // the name of the generated manifest file
+            filename: "manifest.json",
+
+            inject: false,
+
+            fingerprints: false,
+
+            name: "Budget App",
+            short_name: "Budget App",
+            theme_color: "#ffffff",
+            background_color: "#ffffff",
+            start_url: "/",
+            display: "standalone",
+
+            icons: [
+                {
+                    src: path.resolve(
+                        __dirname,
+                        "public/icons/icon-192x192.png"
+                    ),
+                    // the plugin will generate an image for each size
+                    // included in the size array
+                    size: [192, 512]
+                }
+            ]
+        })
+    ]
+}
+
+module.exports = config;
